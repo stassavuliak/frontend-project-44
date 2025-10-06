eeea7469afdbafe754b9dcad77b377deb562c320
Stanislav Savuliak <stassavulak@gmail.com>
2025-10-06 08:26:37 +0200

    fix greeting in cli.js

--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 export const greetUser = () => {
   console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('What is your name? ');
+  const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
 
   return name;
